80d3cc796308a2187008eb2f7b9ecbdea9bb6c1d
Jana <jana.samson@hotmail.fr>
2022-03-16 10:21:22 -0400

    get data from API

---
+++ script.js
@@ -0,0 +1,43 @@
+const movieContainer = document.querySelector(".movie-container")
+
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const IMGPATH = "https://image.tmdb.org/t/p/w500/"
+
+
+async function getMovies() {
+    const response = await fetch(APIURL);
+    const responseData = await response.json();
+
+    console.log( responseData);
+    const { results } = responseData
+    console.log(results)
+
+    results.forEach(result => {
+        const { title, overview, backdrop_path } = result;
+        console.log(backdrop_path)
+
+        const li = document.createElement("li");
+        li.classList.add('movie');
+        movieContainer.appendChild(li);
+
+        const img = document.createElement("img");
+        img.src = IMGPATH + backdrop_path;
+        li.appendChild(img);
+
+        const movieTitle = document.createElement("h4");
+        movieTitle.textContent = title;
+        li.appendChild(movieTitle)
+        
+    })
+}
+
+getMovies();
+
+// function displayMovies() {
+//     let arr = getMovies()
+//     console.log(arr)
+//     const { page, results , total_pages, total_results} = arr
+//     console.log(results)
+// }
+
+// displayMovies();
